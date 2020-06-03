ddc6d6b738cbc5db2da6ca1e7e4a7e847d72ec2c
Jasmin Wilton <45803246+jwilton21@users.noreply.github.com>
2020-06-03 16:13:21 +0100

    Added new method to delete record from database.

--- MySQL-Database-with-Java-JDBC/src/JdbcInsertDemo.java
+++ MySQL-Database-with-Java-JDBC/src/JdbcInsertDemo.java
@@ -1,12 +1,14 @@
+
 // Java SQL library.
 import java.sql.*;
+
 // demo database.
 public class JdbcInsertDemo {
 	// Throwing SQLException if user/pw/database info is incorrect.
 	public static void main(String[] args) throws SQLException {
 		// Initialise database objects- connection, statement, resultSet.
 		Connection myConn = null;
-		Statement myStmt =  null;
+		Statement myStmt = null;
 		ResultSet myRs = null;
 
 		// Assign database connection strings.
@@ -22,44 +24,47 @@ public class JdbcInsertDemo {
 			myStmt = myConn.createStatement();
 
 			// Insert a new employee.
-			System.out.println("Insert a new employee to the database\n");
-
+			System.out.println("Inserting a new employee to the database\n");
 
 			// Update employee table with new record.
-			int rowsAffected = myStmt.executeUpdate(
-					"insert into employees " +
-							"(last_name, first_name, email, department, salary) " +
-							"values " +
-					"('Jones', 'Anna', 'anna.jones@foo.com', 'Legal',28000.00)");
-
-
+			int rowsAffected = myStmt
+					.executeUpdate("insert into employees " + "(last_name, first_name, email, department, salary) "
+							+ "values " + "('Jones', 'Anna', 'anna.jones@foo.com', 'Legal',28000.00)");
 
 			// Verify this was done- get list of employees with select all from.
 
 			myRs = myStmt.executeQuery("select * from employees order by last_name");
 
 			// Process results set.
-			while(myRs.next()) {
-				System.out.println(myRs.getString("last_name")+","+myRs.getString("first_name"));
+			while (myRs.next()) {
+				System.out.println(myRs.getString("last_name") + "," + myRs.getString("first_name"));
 			}
+
 		}
-		// If exception thrown, pass to here and print stack trace if unsuccessful. 
+		// If exception thrown, pass to here and print stack trace if unsuccessful.
 
 		// Should not reach here if connection/details are correct.
-		catch (Exception e) 
-		{
+		catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally 
-		{
-			if (myRs !=null) {
+		} finally {
+			if (myRs != null) {
 				myRs.close();
 			}
 		}
+		System.out.println("Number of rows deleted: " + deleteFromEmployees(0, myStmt));
 
 	}
 
-	// TO DO-- Helper method which will de-dupe records. Too many runs we add dupes.
-	//private static void deDupeRecords()
+	// Use to delete record from database.
+	public static int deleteFromEmployees(int rowsAffected, Statement myStmt) {
+		try {
+			rowsAffected = myStmt
+					.executeUpdate("delete from employees " + "where last_name='Jones' and first_name='Anna'");
+		} catch (SQLException e) {
+			// If exception thrown then print stack trace.
+			e.printStackTrace();
 
+		}
+		return rowsAffected;
+	}
 }
