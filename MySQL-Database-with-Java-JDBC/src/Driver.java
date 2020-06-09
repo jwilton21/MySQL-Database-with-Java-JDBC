40dba135080156e4432a3b05651f7730036d6208
Jasmin Wilton <45803246+jwilton21@users.noreply.github.com>
2020-06-09 14:58:41 +0100

    Added a new method to delete records from database.
    Handles the two exceptions and SQL statement does not involve
    concatenation.
    
    Tested on database that record has been deleted by running create
    statement again and re-run, checked with db instance refresh. 1 record
    missing- 80k and so it is successful.
    
    Fixed curly braces to go on their own line automatically.

--- MySQL-Database-with-Java-JDBC/src/Driver.java
+++ MySQL-Database-with-Java-JDBC/src/Driver.java
@@ -1,3 +1,4 @@
+
 // Java SQL library.
 import java.sql.*;
 
@@ -10,14 +11,17 @@ public class Driver
 		// Initialise database objects- connection, statement, resultSet.
 		Connection myConn = null;
 		PreparedStatement myStmt = null;
+		PreparedStatement myStmtDelete = null;
 		ResultSet myRs = null;
+
 		// Try to connect.
 		try
 		{
 			// 1. Get a connection to database
 			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "student", "student");
 
-			// 2. Prepare statement, calling constructor, pass in SQL. Where salary is >  AND dept = x.
+			// 2. Prepare statement, calling constructor, pass in SQL. Where salary is > AND
+			// dept = x.
 			myStmt = myConn.prepareStatement("select * from employees where salary > ? and department=?");
 
 			// 3. Set the parameters for our statement- before executing.
@@ -27,12 +31,12 @@ public class Driver
 			// 4. Execute SQL query
 			myRs = myStmt.executeQuery();
 
-			// 5. Return result set (all from database where above applicable conditions are met.
+			// 5. Return result set (all from database where above applicable conditions are
+			// met.
 			display(myRs);
 
-			
 			// REUSE prepared statement for other cases.
-			
+
 			// New criteria to be entered into myStmt parameters.
 			// Print out from result set where salary > 3500 AND dept = engineering.
 			System.out.println("\n\nReusing the prepared statement:  salary > 35000,  department = Engineering");
@@ -46,40 +50,71 @@ public class Driver
 
 			// 8. Call method display() to show results.
 			display(myRs);
-			
-		// Catch exception if thrown. Should not reach here.
+
+			// 9. Call delete on record.
+			deleteRecord(myStmtDelete, myConn);
+
+			// Catch exception if thrown. Should not reach here.
 		} catch (Exception exc)
-		{	// Print stack trace if applicable.
+		{ // Print stack trace if applicable.
 			exc.printStackTrace();
 		} finally
-		{	
+		{
 			if (myRs != null)
-			{	// Close result set.
+			{ // Close result set.
 				myRs.close();
 			}
 
 			if (myStmt != null)
-			{	// Close prepared statement.
+			{ // Close prepared statement.
 				myStmt.close();
 			}
 
 			if (myConn != null)
-			{	// Close connection.
+			{ // Close connection.
 				myConn.close();
 			}
 		}
 	}
+
 	// Display results method.
 	private static void display(ResultSet myRs) throws SQLException
-	{	// While resultset has next row do:
+	{ // While resultset has next row do:
 		while (myRs.next())
-		{	// Obtain values from database.
+		{ // Obtain values from database.
 			String lastName = myRs.getString("last_name");
 			String firstName = myRs.getString("first_name");
 			double salary = myRs.getDouble("salary");
 			String department = myRs.getString("department");
 			// Print out values from database in this order.
 			System.out.printf("%s, %s, %.2f, %s\n", lastName, firstName, salary, department);
+		}
+
+	}
+	// This method deletes records.
+	private static void deleteRecord(Statement myStmtDelete, Connection myConn) throws SQLException
+	{
+		try
+		{	// Create the sql statement.
+			myStmtDelete = myConn.createStatement();
+			// Execute the statement.
+			myStmtDelete.execute("DELETE FROM EMPLOYEES WHERE SALARY >= 80000");
+
+			// Catch sql exception.
+		} catch (SQLException e)
+		{ // Print stack trace if applicable.
+			e.printStackTrace();
+		} finally
+		{	// Another try catch block to catch class exceptions.
+			try
+			{ // Close objects.
+				myStmtDelete.close();
+				myConn.close();
+			} catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+
 		}
 	}
 }
